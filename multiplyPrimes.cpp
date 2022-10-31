8d57fa4304a9eed0ef89bcdea0219ba0b4480a29
JoyKevin1 <banjoflylive@gmail.com>
2022-10-31 13:40:58 +0200

    Multiplying prime numbers App Finished

--- multiplyPrimes.cpp
+++ multiplyPrimes.cpp
@@ -1,11 +1,34 @@
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int main() {
-//     int primeNumber = {};
-//     for(int i = 2; i < 99; i++) {
-//         for(int j = 1; j < i; j++) {
-//          if(i )
-//         }
-//     }
-// }
+int main()
+{
+    int product = 1;
+    int found = 0;
+    int primeNumber[25] = {};
+
+    for (int i = 2; i <= 97; i++)
+    {
+        int count = 0;
+        for (int j = 1; j <= i; j++)
+        {
+            if (i % j == 0)
+            {
+                count += 1;
+            }
+        }
+        if (count == 2)
+        {
+            primeNumber[found] = i;
+            found++;
+        }
+    }
+
+    for (int k; k < 25; k++)
+    {
+        product *= primeNumber[k];
+    }
+
+    cout << "The product of prime numbers between 1 and 100 is " << product << endl;
+    return 0;
+}
